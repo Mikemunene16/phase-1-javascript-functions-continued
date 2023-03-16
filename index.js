9637d64bc3b59aa70db84880e78b0f2f527976fe
Mike <mike.munene@student.moringaschool.com>
2023-03-16 15:39:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(string = "roller-skate") {
+    return `This Saturday, I want to ${string}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symb = "*") {
+    return function (string = "special") {
+        return `You are ${symb}${string}${symb}!`;
+    }
+}
